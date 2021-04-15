305991aec1c5cf8858a439d5d8e7549bfccfb9e3
Toan Dao <daomanhtoan996t@gmail.com>
2021-04-15 13:19:31 +0700

    Complete newArticle & myProfile

--- src/app/components/views/my-profile/my-profile.component.ts
+++ src/app/components/views/my-profile/my-profile.component.ts
@@ -1,10 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Article } from 'src/app/models/articles';
-import { ArticleService } from 'src/app/services/ArticleService/article.service';
-import { AuthService } from 'src/app/services/AuthService/auth.service';
-import { MyArticleComponent } from '.././my-profile/my-article/my-article.component';
-import { FavoritedArticleComponent } from '.././my-profile/favorited-article/favorited-article.component';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/UserService/user.service';
 
@@ -15,18 +9,17 @@ import { UserService } from 'src/app/services/UserService/user.service';
 })
 export class MyProfileComponent implements OnInit {
   currentUser;
-  userImage;
+  userInfo;
   showActiveTab;
-  constructor(
-    private articleService: ArticleService,
-    private auth: AuthService,
-    private router: Router,
-    private userService: UserService
-  ) {}
+  constructor(private router: Router, private userService: UserService) {}
 
   ngOnInit(): void {
     this.currentUser = '';
+    this.userInfo = {};
     this.userService.getUser().subscribe((res) => {
+      this.userInfo = res['user'];
+      console.log(this.userInfo);
+
       this.userService
         .getProfile(res['user'].username)
         .subscribe((res: any) => {
